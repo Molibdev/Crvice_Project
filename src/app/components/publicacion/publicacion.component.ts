c31feb278cf8deb42faec8f3326f7d5c0ab79475
Sebastian Moya <se.moya@duocuc.cl>
2023-05-23 18:20:49 -0400

    Cambios en Ver publicacion

--- src/app/components/publicacion/publicacion.component.ts
+++ src/app/components/publicacion/publicacion.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { PublicacionesService } from 'src/app/services/publicaciones.service'; 
-import { Publicacion } from 'src/app/models/publicacion'; 
+import { PublicacionesService } from 'src/app/services/publicaciones.service';
+import { Publicacion } from 'src/app/models/publicacion';
 import { InteractionService } from 'src/app/services/interaction.service';
-import { User } from 'src/app/models/models';
+import { Calificacion, User } from 'src/app/models/models';
 import { combineLatest, map, of, startWith, switchMap } from 'rxjs';
 import { FormControl } from '@angular/forms';
 import { FirebaseService } from 'src/app/services/firebase.service';
@@ -15,10 +15,17 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 })
 export class PublicacionComponent implements OnInit {
   publicacion?: Publicacion;
+  usuarioLogueado: boolean = false; // Variable para controlar si el usuario ha iniciado sesión
+  averageRating: number = 0; // Variable para almacenar la calificación promedio
+  contador: number = 1;
+  mostrarCargarMenos: boolean = false;
+  ratings: Calificacion[] = [];
 
   user$ = this.firebase.currentUserProfile$;
   searchControl = new FormControl('');
   public fotos: string[] = [];
+  fotoActual: string | undefined; // URL de la foto actualmente mostrada
+  indiceFotoActual: number = 0; // Índice de la foto actual
 
   users$ = combineLatest([this.firebase.allUsers$, this.user$, this.searchControl.valueChanges.pipe(startWith(''))]).pipe(
     map(([users, user, searchString]) => users.filter(u => u.nombre?.toLowerCase().includes(searchString?.toLowerCase() ?? '') && u.uid !== user?.uid))
@@ -35,36 +42,58 @@ export class PublicacionComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    // Verificar si el usuario ha iniciado sesión
+    this.firebase.currentUserProfile$.subscribe(user => {
+      this.usuarioLogueado = !!user;
+    });
+  
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.publicacionesService.getPublicacion(id).subscribe(publicacion => {
         this.publicacion = publicacion;
         localStorage.setItem('publicacionId', id);
-      // Obtener las URL de las fotos
-      this.firebase.getFotosURL(publicacion.uid, id).subscribe(urls => {
-        Promise.all(urls).then(resolvedUrls => {
-          this.fotos = resolvedUrls;
+  
+        // Obtener las URL de las fotos
+        this.firebase.getFotosURL(publicacion.uid, id).subscribe(urls => {
+          Promise.all(urls).then(resolvedUrls => {
+            this.fotos = resolvedUrls;
+  
+            if (this.fotos.length > 0) {
+              this.fotoActual = this.fotos[0];
+            }
+          });
         });
+  
+        // Obtener las calificaciones y comentarios
+        const path = `Usuarios/${publicacion.uid}/calificaciones`;
+        this.firebase.getCollection<Calificacion>(path).subscribe(calificaciones => {
+          this.ratings = calificaciones;
+        });
+  
+        // Calcular la calificación promedio
+        this.calculateAverageRating(publicacion.uid);
       });
-    });
+    }
   }
-  }
+  
 
   createChat(otherUser: User) {
-    this.chat.isExistingChat(otherUser?.uid).pipe(
-      switchMap(chatId => {
-        if (chatId) {
-          return of(chatId);
-        } else {
-          return this.chat.createChat(otherUser);
-        }
-      })
-    ).subscribe(chatId => {
-      this.chatListControl.setValue([chatId]);
-    })
-    this.router.navigate(['/chat'])
+    this.chat
+      .isExistingChat(otherUser?.uid)
+      .pipe(
+        switchMap(chatId => {
+          if (chatId) {
+            return of(chatId);
+          } else {
+            return this.chat.createChat(otherUser);
+          }
+        })
+      )
+      .subscribe(chatId => {
+        this.chatListControl.setValue([chatId]);
+      });
+    this.router.navigate(['/chat']);
   }
-
 
   solicitarTrabajo() {
     const id = this.publicacion?.id;
@@ -75,5 +104,49 @@ export class PublicacionComponent implements OnInit {
       console.log('currentUserUid:', currentUserUid);
       this.router.navigate(['/solicitar-trabajo', id, uid, currentUserUid]);
     });
+  }
+
+  mostrarImagenAnterior() {
+    this.indiceFotoActual--;
+    if (this.indiceFotoActual < 0) {
+      this.indiceFotoActual = this.fotos.length - 1;
+    }
+    this.fotoActual = this.fotos[this.indiceFotoActual];
+  }
+
+  mostrarImagenSiguiente() {
+    this.indiceFotoActual++;
+    if (this.indiceFotoActual >= this.fotos.length) {
+      this.indiceFotoActual = 0;
+    }
+    this.fotoActual = this.fotos[this.indiceFotoActual];
+  }
+
+  calculateAverageRating(uid: string) {
+    const path = `Usuarios/${uid}/calificaciones`;
+    this.firebase
+      .getCollection<Calificacion>(path)
+      .subscribe(calificaciones => {
+        const ratings = calificaciones;
+        let totalRating = 0;
+        for (const rating of ratings) {
+          totalRating += +rating.calificacion; // Convertir a número utilizando el prefijo '+'
+        }
+        console.log('Calificación Total:', totalRating);
+        console.log('Cantidad de Calificaciones:', ratings.length);
+        this.averageRating = ratings.length > 0 ? totalRating / ratings.length : 0;
+        console.log('Calificación Promedio:', this.averageRating);
+      });
+  }
+
+  cargarMasComentarios() {
+    this.contador += 5;
+    this.mostrarCargarMenos = true;
+
+  }
+  cargarMenosComentarios() {
+    if (this.contador=1){
+      this.mostrarCargarMenos = false;
+    }
   }
 }
